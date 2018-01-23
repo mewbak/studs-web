1607f0285ab8c47d8385fab6266fe3567d007361
Jesper Sandström <jespersandstrom@gmail.com>
2018-01-23 19:32:53 +0100

    Temp removal of hotloader

--- app/index.js
+++ app/index.js
@@ -2,23 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { AppContainer } from 'react-hot-loader'
 import registerServiceWorker from './registerServiceWorker'
 
 const render = (Component) => {
   ReactDOM.render((
-    <AppContainer>
-      <Component />
-    </AppContainer>
+    <Component />
   ), document.getElementById('root'))
 }
 
 render(App)
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App)
-  })
-}
 
 registerServiceWorker()
